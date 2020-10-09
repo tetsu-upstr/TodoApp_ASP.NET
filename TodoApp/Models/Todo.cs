e451f83b74d43c5fe6a283db0ac15a868a548edc
tetsu-upstr <bodomu@gmail.com>
2020-10-09 12:58:39 +0900

    first commit

---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace TodoApp.Models
+{
+
+    // クラス名は必ず単数形
+    public class Todo
+    {
+        public int Id { get; set; } // Idは自動的に主キーになる
+        public string Summary  { get; set; }
+        public string Detail { get; set; }
+        public DateTime Limit { get; set; }
+        public bool Done { get; set; }
+    }
+}
